3b942f9803a9b3da57d7f26d2ef150a1675fc27e
Nikita Grigorev <pettyaelse@gmail.com>
2025-05-04 23:44:06 +0700

    w

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,10 +1,5 @@
 ﻿namespace TelegramBot;
 
-using Telegram.Bot;
-using Telegram.Bot.Types;
-using Telegram.Bot.Polling;
-using Telegram.Bot.Types.Enums;
-
 class Program
 {
     static async Task Main()
@@ -18,40 +13,7 @@ class Program
 
         Console.WriteLine($"Token length is {token.Length}");
 
-        var botClient = new TelegramBotClient(token);
-
-        using var cts = new CancellationTokenSource();
-
-        var receiverOptions = new ReceiverOptions
-        {
-            AllowedUpdates = [],
-        };
-
-        botClient.StartReceiving(
-            updateHandler: HandleUpdateAsync,
-            errorHandler: HandleErrorAsync,
-            receiverOptions: receiverOptions,
-            cancellationToken: cts.Token);
-
-        var me = await botClient.GetMe(cancellationToken: cts.Token);
-        Console.WriteLine($"Start listening @{me.Username}");
-        
-        cts.Token.WaitHandle.WaitOne();
-    }
-
-    private static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update,
-        CancellationToken cancellationToken)
-    {
-        if (update.Message is not { Text: { } messageText }) return;
-
-        var chatId = update.Message.Chat.Id;
-        await botClient.SendMessage(chatId, messageText, cancellationToken: cancellationToken);
-    }
-
-    private static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception,
-        CancellationToken cancellationToken)
-    {
-        Console.WriteLine($"Error: {exception.Message}");
-        return Task.CompletedTask;
+        var bot = new WeightBot(token);
+        await bot.Run();
     }
 }
---
+++ TelegramBot/WeightBot.cs
@@ -0,0 +1,57 @@
+﻿namespace TelegramBot;
+
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+
+public class WeightBot
+{
+    private string _token;
+
+    public WeightBot(string token)
+    {
+        _token = token;
+    }
+
+
+    public async Task Run()
+    {
+        var botClient = new TelegramBotClient(_token);
+
+        using var cts = new CancellationTokenSource();
+
+        var receiverOptions = new ReceiverOptions
+        {
+            AllowedUpdates = [],
+        };
+
+        botClient.StartReceiving(
+            updateHandler: HandleUpdateAsync,
+            errorHandler: HandleErrorAsync,
+            receiverOptions: receiverOptions,
+            cancellationToken: cts.Token);
+
+        var me = await botClient.GetMe(cancellationToken: cts.Token);
+        Console.WriteLine($"Start listening @{me.Username}");
+
+        cts.Token.WaitHandle.WaitOne();
+    }
+
+
+    private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update,
+        CancellationToken cancellationToken)
+    {
+        if (update.Message is not { Text: { } messageText }) return;
+
+        var chatId = update.Message.Chat.Id;
+        await botClient.SendMessage(chatId, messageText, cancellationToken: cancellationToken);
+    }
+
+    private Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception,
+        CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Error: {exception.Message}");
+        return Task.CompletedTask;
+    }
+}
