8de8eb70ab09d700b835bc4660f842316cf27568
Nikita Grigorev <pettyaelse@gmail.com>
2025-05-04 23:12:07 +0700

    w

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        string? tokenVar = Environment.GetEnvironmentVariable("BOT_TOKEN");
+        if (string.IsNullOrEmpty(tokenVar))
+        {
+            Console.WriteLine("Environment variable 'BOT_TOKEN' is not set");
+            return;
+        }
+
+        string token = tokenVar;
+
+
         Console.WriteLine("Hello, World!");
     }
 }
