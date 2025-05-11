5ac4d3afe4b90202c2e6841d4745c54fe6ef9961
Nikita Grigorev <pettyaelse@gmail.com>
2025-05-11 16:16:21 +0700

    w

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -22,14 +22,14 @@ class Program
 
         // TODO: check versions
 
-        string createUsersSql = @"
-CREATE TABLE IF NOT EXISTS users
-(
-    id            INTEGER PRIMARY KEY,
-    register_date TEXT NOT NULL,
-    wanted_ccal   REAL
-);
-";
+        string createUsersSql = """
+                                CREATE TABLE IF NOT EXISTS users
+                                (
+                                    id            INTEGER PRIMARY KEY,
+                                    register_date TEXT NOT NULL,
+                                    wanted_ccal   REAL
+                                );
+                                """;
         var createUsersCmd = new SQLiteCommand(createUsersSql, connection);
         createUsersCmd.ExecuteNonQuery();
 
