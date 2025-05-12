760e4c86ca6c915a96d1f1a9d4a9078ce4fd2803
Nikita Grigorev <pettyaelse@gmail.com>
2025-05-12 23:02:45 +0700

    w

--- TelegramBot/WeightBot.cs
+++ TelegramBot/WeightBot.cs
@@ -158,6 +158,6 @@ public partial class WeightBot
         return dateTime.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
     }
 
-    [GeneratedRegex(@"^\s*/(?P<cmd>\w+)(?:\s+(?P<args>.+?))?\s*$")]
+    [GeneratedRegex(@"^\s*/(?<cmd>\w+)(?:\s+(?<args>.+?))?\s*$")]
     private static partial Regex GetParseCommandRegex();
 }
