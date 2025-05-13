9fb5a0902722f5e03384beb1738eb658616baa58
Nikita Grigorev <pettyaelse@gmail.com>
2025-05-13 21:37:30 +0700

    w

---
+++ TelegramBot/BotDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.SQLite;
+
+namespace TelegramBot;
+
+public class BotDatabase : IDisposable
+{
+    private readonly SQLiteConnection _connection;
+    private bool _disposed;
+
+    public BotDatabase(string databasePath)
+    {
+        string connectionString = $"Data Source={databasePath};Version=3;";
+        
+        _connection = new SQLiteConnection(connectionString);
+        // await connection.OpenAsync();
+    }
+    
+    
+
+    void IDisposable.Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposed) return;
+        if (disposing)
+        {
+            _connection.Dispose();
+        }
+
+        _disposed = true;
+    }
+}
