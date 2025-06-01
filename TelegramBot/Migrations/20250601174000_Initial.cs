eea83a555067e9a48bfc29987adb81e8d8ad1146
Nikita Grigorev <pettyaelse@gmail.com>
2025-06-02 00:40:44 +0700

    mig

---
+++ TelegramBot/Migrations/20250601174000_Initial.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TelegramBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    user_id = table.Column<long>(type: "INTEGER", nullable: false),
+                    register_date = table.Column<string>(type: "TEXT", nullable: false),
+                    date_time_offset = table.Column<int>(type: "INTEGER", nullable: true),
+                    min_kcal = table.Column<double>(type: "REAL", nullable: true),
+                    max_kcal = table.Column<double>(type: "REAL", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.user_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "consumed",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    user_id = table.Column<long>(type: "INTEGER", nullable: false),
+                    date = table.Column<string>(type: "TEXT", nullable: false),
+                    text = table.Column<string>(type: "TEXT", nullable: false),
+                    kcal = table.Column<double>(type: "REAL", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_consumed", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_consumed_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_consumed_user_id",
+                table: "consumed",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "consumed");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
