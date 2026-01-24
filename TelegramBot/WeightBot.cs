9f93a50603ed3bcca40b3451a30599f29f15ad74
Nikita Grigorev <pettyaelse@gmail.com>
2026-01-24 23:45:37 +0700

    w

--- TelegramBot/WeightBot.cs
+++ TelegramBot/WeightBot.cs
@@ -109,7 +109,7 @@ public partial class WeightBot
 
         int timezone = await _database.GetUserTimezoneOffsetAsync(userid);
         DateTime localTime = now.AddHours(timezone);
-        
+        int hour = localTime.Hour;
 
         string message = "hi!";
         await botClient.SendMessage(userid, message, cancellationToken: cancellationToken);
